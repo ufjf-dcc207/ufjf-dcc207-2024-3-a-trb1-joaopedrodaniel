6984e02f14a152d8180927578df07810ea4fb7ad
Caio Reis <caiofreis2005@gmail.com>
2024-12-13 20:22:39 -0300

    Cria componentes navbar e footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 //import './App.css'
 
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+
 function App() {
-  return ("Olá Mundo!");
+  return (
+    <div>
+      <Navbar />
+      <Footer />
+    </div>
+  );
 }
 
 export default App
---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,11 @@
+import './Footer.css'; // Opcional: para adicionar estilos
+
+function Footer() {
+    return (
+        <footer className="footer">
+            <p>&copy; 2023 Meu Projeto. Todos os direitos reservados.</p>
+        </footer>
+    );
+};
+
+export default Footer;
