cada1d544b733cf6fbaa7f279e66cfbd40d6bfaa
Luiza Caldeira Daniel <luiza.caldeira@estudante.ufjf.br>
2024-12-14 15:54:28 -0300

    Adiciona o componente no App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 //import './App.css'
 
 import Footer from "./components/Footer";
+import HeroSection from "./components/HeroSection";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div>
       <Navbar />
+      <HeroSection/>
       <Footer />
     </div>
   );
