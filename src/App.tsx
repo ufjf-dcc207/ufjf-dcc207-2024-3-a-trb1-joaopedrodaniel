8f1422a6ae206e3ba01f43867723efaa1d4f808a
Luiza Caldeira Daniel <luiza.caldeira@estudante.ufjf.br>
2024-12-14 17:37:08 -0300

    Adiciona o componente Sobre no arquivo App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
-//import './App.css'
-
+import './App.css'
 import Footer from "./components/Footer";
 import HeroSection from "./components/HeroSection";
 import Navbar from "./components/Navbar";
+import Sobre from "./components/Sobre";
 
 function App() {
   return (
-    <div>
+    <div className="app">
       <Navbar />
       <HeroSection/>
+      <Sobre/>
       <Footer />
     </div>
   );
