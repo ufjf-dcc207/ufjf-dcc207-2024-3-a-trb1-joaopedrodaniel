c00b6d4a0130bb163172db8db623a900a469d74e
Caio Reis <caiofreis2005@gmail.com>
2024-12-14 13:40:24 -0300

    Estiliza o componente navbar com cores e fontes específicas

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,13 +1,12 @@
-import './Navbar.css'; // Opcional: para adicionar estilos
+import './Navbar.css';
 
 function Navbar() {
     return (
         <nav className="navbar">
+            <img src="/img/1.png" alt="Logo" className="navbar-logo" />
             <ul className="navbar-list">
-                <li className="navbar-item"><a href="/">Home</a></li>
-                <li className="navbar-item"><a href="/about">About</a></li>
-                <li className="navbar-item"><a href="/services">Services</a></li>
-                <li className="navbar-item"><a href="/contact">Contact</a></li>
+                <li className="navbar-item"><a href="#">CARDÁPIO</a></li>
+                <li className="navbar-item"><a href="#">SOBRE NÓS</a></li>
             </ul>
         </nav>
     );
