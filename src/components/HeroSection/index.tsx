b5f3271a01c5d83c740229715385427fb52bb5d1
Luiza Caldeira Daniel <luiza.caldeira@estudante.ufjf.br>
2024-12-14 15:52:51 -0300

    Cria componente HeroSection

---
+++ src/components/HeroSection/index.tsx
@@ -0,0 +1,11 @@
+import './HeroSection.css';
+
+export default function HeroSection (){
+    return (
+        <div className="hero-section">
+            <div className="inner">
+                <h1>o sabor que você merece.</h1>
+            </div>
+        </div>
+    );
+}
